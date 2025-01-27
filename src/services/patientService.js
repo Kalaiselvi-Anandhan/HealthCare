286d67e06e451b4033199d2e509c653316293f98
Kalaiselvi-Anandhan <62936416+Kalaiselvi-Anandhan@users.noreply.github.com>
2025-01-27 18:22:51 +0530

    Updates Service for Patients to display all patients

--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,16 +1,44 @@
 const models = require('../models');
 
+const getPatients = async () => {
+    const result = await models.Patients.findAndCountAll()
+    return result
+}
+
 const getPatient = async (patientId) => {
-    const result = await models.Patients.findByPk(patientId)
+    const result = await models.Patients.findByPk(patientId,{
+        exclude:['password']})
     return result
 }
 
 const CreatePatient = async (payload) => {
-    const result = await models.Patients.create(payload)
+    const result = await models.Patients.create(payload,{ fields: ['first_name','email','phone','daily_goals','dob'] })
+    return result
+}
+
+const updatePatient = async (payload, patientId) => {
+    const result = await models.Patients.update(payload, {
+        where:{
+            patient_id:patientId
+        }
+    })
+    return result
+}
+
+const updatePatientWellness = async (payload, patientId) => {
+    const result = await models.Patients.update(payload, {
+        where:{
+            patient_id:patientId
+        }
+    })
     return result
 }
 
 module.exports = {
     getPatient,
     CreatePatient,
+    updatePatient,
+    updatePatientWellness,
+    getPatients,
+
 }

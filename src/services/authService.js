fdca1cc018284c7cd961d169fede4d27bcfaaccb
Kalaiselvi-Anandhan <62936416+Kalaiselvi-Anandhan@users.noreply.github.com>
2025-01-27 18:46:44 +0530

    excludes unwanted data

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,8 +4,6 @@ const bcrypt = require('bcrypt');
 const configData = require('../configs/env');
 const { generateToken } = require('../utils/token');
 
-
-
 const Login = async (payload) => {
     const { type } = payload;
     const ModelType = type === 'patient' ? 'Patients' : 'Providers';
@@ -16,13 +14,10 @@ const Login = async (payload) => {
             email: payload.email
         }
     })
-    console.log("user", JSON.stringify(user,null,1))
-    const match = await bcrypt.compare(payload.password, user.password) // user.password === payload.password
+    const match = await bcrypt.compare(payload.password, user.password)
     if(match) {
         token = generateToken({ email: user.email, id: user[TypeId], name: user.first_name, type: 'patient' })
-        // jwt.sign({ email: user.email, id: user[TypeId], name: user.first_name, type: 'patient' }, configData.secret_key)
     }
-    console.log("result", token)
 
     return {token, id: user[TypeId], first_name: user.first_name, email: user.email}
 }
@@ -30,10 +25,11 @@ const Login = async (payload) => {
 const Register = async (payload) => {
     const { type } = payload;
     const ModelType = type === 'patient' ? 'Patients' : 'Providers'
+    const TypeId = type === 'patient' ? 'patient_id' : 'provider_id'
     payload.password = bcrypt.hashSync(payload.password, 10)
     const result = await models[ModelType].create(payload)
-    console.log("registered!!", result, payload.password)
-    return result
+
+    return {id: result[TypeId], first_name:result.first_name, email:result.email}
 }
 
 module.exports = {
